8946be646db609a4570b6cdb32bce57b8b5ccd57
Anthony Lieuallen <arantius@gmail.com>
2011-09-27 22:42:06 -0400

    mostly broken autocomplete directive

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+// http://polishinggems.blogspot.com/2011/09/building-angularjs-directive-for-jquery_01.html
+angular.directive('my:autocomplete', function(expression, element) {
+  var compiler = this;
+  return function(element) {
+    var currentScope = this;
+    element.autocomplete({'source': window[expression], 'select': function(a,b,c) {
+      //console.log('comp select', currentScope, currentScope.$root);
+      //console.log(a, b, c);
+      //currentScope.$eval('produce_persona="'+b.value+'"');
+      // ARGH HOW??
+    }});
+  }
+});
+
 function CalcCtrl() {
 }
 CalcCtrl.$inject = [];
 
 CalcCtrl.prototype.getRecipes = function() {
-  console.log('get recipes for', this.produce_name)
+  if ('undefined' == typeof personaeByName[this.produce_persona]) {
+    return [];
+  }
+
+  return ['a'];
 };
