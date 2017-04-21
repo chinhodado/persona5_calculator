99999ef534fc1b4c888d79332859aa1f0d5067d9
Trung Do <chin.bimbo@gmail.com>
2017-04-20 23:14:48 -0400

    Move views to their own folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 ///<reference path="PersonaController.ts"/>
 ///<reference path="PersonaListController.ts"/>
 ///<reference path="SkillListController.ts"/>
+///<reference path="SettingController.ts"/>
 var StickyTableDirective = function () { return ({
     restrict: 'A',
     link: function ($scope, $element) {
@@ -15,10 +16,10 @@ myModule.directive('stickyTable', StickyTableDirective);
 myModule.controller('PersonaController', ['$scope', PersonaController]);
 myModule.controller('PersonaListController', ['$scope', PersonaListController]);
 myModule.config(function ($routeProvider) {
-    $routeProvider.when('/list', { templateUrl: 'list.html', controller: PersonaListController });
-    $routeProvider.when('/skill', { templateUrl: 'skill.html', controller: SkillListController });
-    $routeProvider.when('/persona/:persona_name', { templateUrl: 'persona.html', controller: PersonaController });
-    $routeProvider.when('/setting', { templateUrl: 'setting.html', controller: SettingController });
+    $routeProvider.when('/list', { templateUrl: 'view/list.html', controller: PersonaListController });
+    $routeProvider.when('/skill', { templateUrl: 'view/skill.html', controller: SkillListController });
+    $routeProvider.when('/persona/:persona_name', { templateUrl: 'view/persona.html', controller: PersonaController });
+    $routeProvider.when('/setting', { templateUrl: 'view/setting.html', controller: SettingController });
 });
 myModule.run(function ($rootScope, $location, $route, $window) {
     $rootScope.$on('$locationChangeStart', function (event) {
